7f9e5a773c88ae9a18767c4737141416ab5cb52e
Ramon Arca (PC) <ramonteodoroarca@gmail.com>
2022-07-22 23:27:30 +0800

    Add calculator functions to JS script

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
 
+function add(x, y){
+  return x + y;
+}
+
+function minus(x, y){
+  return x - y;
+}
+
+function multiply(x, y){
+  return x * y;
+}
+
+function divide(x, y){
+  return x / y;
+}
+
+function operate(num1, num2, operation){
+  
+  switch(operation){
+    case '+':
+      return add(num1, num2); break;
+    case '-':
+      return minus(num1, num2); break;
+    case '*':
+      return multiply(num1, num2); break;
+    case '/':
+      return divide(num1, num2); break;
+  }
+  
+}
+
+
+
+
+/* 
+  Code part for adding eventlisteners to the calculator buttons
+  TODO: add saving of values to an Object
+*/
 const buttons = document.querySelectorAll('.calculator-button');
 
 buttons.forEach((button) => {
